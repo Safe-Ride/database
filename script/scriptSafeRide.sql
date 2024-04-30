138aa7fc58aedc08fdfae1683c5066f0768d569a
Bruno Henrique <bruno.lima@sptech.school>
2024-04-29 23:12:14 -0300

    fix: script no geral

--- script/scriptSafeRide.sql
+++ script/scriptSafeRide.sql
@@ -4,313 +4,77 @@ FLUSH PRIVILEGES;
 
 CREATE DATABASE IF NOT EXISTS saferide;
 use saferide;
--- -----------------------------------------------------
--- Table `Transacao`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Transacao` (
-  `id` INT NOT NULL,
-  `valor` DECIMAL(5,2) NULL,
-  `dia` DATETIME NULL,
-  `contaDebito` VARCHAR(20) NULL,
-  `contaDeposito` VARCHAR(20) NULL,
-  `fkDebito` INT NOT NULL,
-  `fkDeposito` INT NOT NULL,
-  PRIMARY KEY (`id`));
-
 
 -- -----------------------------------------------------
--- Table `Usuario`
+-- Table `usuario`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Usuario` (
-  `id` INT NOT NULL,
-  `nome` VARCHAR(45) NULL,
-  `email` VARCHAR(45) NULL,
-  `senha` CHAR(16) NULL,
-  `CPF` CHAR(11) NULL,
-  `telefone` CHAR(11) NULL,
-  `data_nascimento` DATE NULL,
-  `tipo` INT NULL,
-  PRIMARY KEY (`id`));
-
-
--- -----------------------------------------------------
--- Table `Dependente`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Dependente` (
-  `id` INT NOT NULL,
-  `nome` VARCHAR(45) NULL,
-  `data_nascimento` DATE NULL,
-  `escola` VARCHAR(45) NULL,
-  `serie` VARCHAR(45) NULL,
-  `responsavel_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_Dependente_Cliente1_idx` (`responsavel_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Dependente_Cliente1`
-    FOREIGN KEY (`responsavel_id`)
-    REFERENCES `Usuario` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
-
-
--- -----------------------------------------------------
--- Table `Transporte`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Transporte` (
-  `id` INT NOT NULL,
-  `placa` VARCHAR(45) NULL,
-  `cnpj` CHAR(14) NULL,
-  `cnh` CHAR(9) NULL,
-  `crm` VARCHAR(15) NULL,
-  `crmc` VARCHAR(20) NULL,
-  `motorista_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_Transporte_Cliente_idx` (`motorista_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Transporte_Cliente`
-    FOREIGN KEY (`motorista_id`)
-    REFERENCES `Usuario` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
-
-
--- -----------------------------------------------------
--- Table `Endereco`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Endereco` (
-  `id` INT NOT NULL,
-  `latitude` VARCHAR(45) NULL,
-  `longitude` VARCHAR(45) NULL,
-  `cep` CHAR(8) NULL,
-  `numero` DECIMAL(4) NULL,
-  `complemento` VARCHAR(45) NULL,
-  `usuario_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_Endereco_Usuario1_idx` (`usuario_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Endereco_Usuario1`
-    FOREIGN KEY (`usuario_id`)
-    REFERENCES `Usuario` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
-
-
--- -----------------------------------------------------
--- Table `Trajeto`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Trajeto` (
-  `id` INT NOT NULL,
-  `escola` VARCHAR(45) NULL,
-  `tipo` VARCHAR(45) NULL,
-  `diaSemana` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`));
-
-
--- -----------------------------------------------------
--- Table `Chat`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Chat` (
-  `id` INT NOT NULL,
-  `responsavel_id` INT NOT NULL,
-  `motorista_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `motorista_id`, `responsavel_id`),
-  INDEX `fk_Chat_Cliente1_idx` (`responsavel_id` ASC) VISIBLE,
-  INDEX `fk_Chat_Cliente2_idx` (`motorista_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Chat_Cliente1`
-    FOREIGN KEY (`responsavel_id`)
-    REFERENCES `Usuario` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Chat_Cliente2`
-    FOREIGN KEY (`motorista_id`)
-    REFERENCES `Usuario` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
-
-
--- -----------------------------------------------------
--- Table `Mensagem`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Mensagem` (
-  `id` INT NOT NULL,
-  `data` DATETIME NULL,
-  `conteudo` VARCHAR(45) NULL,
-  `chat_id` INT NOT NULL,
-  `cliente_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_Mensagem_Chat1_idx` (`chat_id` ASC) VISIBLE,
-  INDEX `fk_Mensagem_Cliente1_idx` (`cliente_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Mensagem_Chat1`
-    FOREIGN KEY (`chat_id`)
-    REFERENCES `Chat` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Mensagem_Cliente1`
-    FOREIGN KEY (`cliente_id`)
-    REFERENCES `Usuario` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
-
-
--- -----------------------------------------------------
--- Table `TrajetoDependente`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `TrajetoDependente` (
-  `trajeto_id` INT NOT NULL,
-  `dependente_id` INT NOT NULL,
-  `endereco_id` INT NOT NULL,
-  PRIMARY KEY (`trajeto_id`, `dependente_id`, `endereco_id`),
-  INDEX `fk_Trajeto_has_Dependente_Dependente1_idx` (`dependente_id` ASC) VISIBLE,
-  INDEX `fk_Trajeto_has_Dependente_Trajeto1_idx` (`trajeto_id` ASC) VISIBLE,
-  INDEX `fk_TrajetoDependente_Endereco1_idx` (`endereco_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Trajeto_has_Dependente_Trajeto1`
-    FOREIGN KEY (`trajeto_id`)
-    REFERENCES `Trajeto` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Trajeto_has_Dependente_Dependente1`
-    FOREIGN KEY (`dependente_id`)
-    REFERENCES `Dependente` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_TrajetoDependente_Endereco1`
-    FOREIGN KEY (`endereco_id`)
-    REFERENCES `Endereco` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
-
--- -----------------------------------------------------
--- Table `endereco`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `endereco` (
-  `id` INT NOT NULL,
-  `latitude` VARCHAR(45) NULL DEFAULT NULL,
-  `longitude` VARCHAR(45) NULL DEFAULT NULL,
-  `cep` CHAR(8) NULL DEFAULT NULL,
-  `numero` DECIMAL(4,0) NULL DEFAULT NULL,
-  `complemento` VARCHAR(45) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`));
-
-
--- -----------------------------------------------------
--- Table `cliente`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cliente` (
-  `id` INT NOT NULL,
-  `nome` VARCHAR(45) NULL DEFAULT NULL,
-  `email` VARCHAR(45) NULL DEFAULT NULL,
-  `senha` CHAR(16) NULL DEFAULT NULL,
-  `CPF` CHAR(11) NULL DEFAULT NULL,
-  `telefone` CHAR(11) NULL DEFAULT NULL,
-  `data_nascimento` DATE NULL DEFAULT NULL,
-  `tipo` INT NULL DEFAULT NULL,
-  `endereco_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_Cliente_Endereco1_idx` (`endereco_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Cliente_Endereco1`
-    FOREIGN KEY (`endereco_id`)
-    REFERENCES `endereco` (`id`));
-
-
--- -----------------------------------------------------
--- Table `chat`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `chat` (
-  `id` INT NOT NULL,
-  `responsavel_id` INT NOT NULL,
-  `motorista_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `motorista_id`, `responsavel_id`),
-  INDEX `fk_Chat_Cliente1_idx` (`responsavel_id` ASC) VISIBLE,
-  INDEX `fk_Chat_Cliente2_idx` (`motorista_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Chat_Cliente1`
-    FOREIGN KEY (`responsavel_id`)
-    REFERENCES `cliente` (`id`),
-  CONSTRAINT `fk_Chat_Cliente2`
-    FOREIGN KEY (`motorista_id`)
-    REFERENCES `cliente` (`id`));
-
+CREATE TABLE IF NOT EXISTS `usuario` (
+	`id` INT NOT NULL,
+	`nome` VARCHAR(45) NULL,
+	`email` VARCHAR(45) NULL,
+	`senha` CHAR(16) NULL,
+	`CPF` CHAR(11) NULL,
+	`telefone` CHAR(11) NULL,
+	`data_nascimento` DATE NULL,
+	`tipo` INT NULL,
+	PRIMARY KEY (`id`)
+);
 
 -- -----------------------------------------------------
 -- Table `dependente`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `dependente` (
-  `id` INT NOT NULL,
-  `nome` VARCHAR(45) NULL DEFAULT NULL,
-  `data_nascimento` DATE NULL DEFAULT NULL,
-  `escola` VARCHAR(45) NULL DEFAULT NULL,
-  `serie` VARCHAR(45) NULL DEFAULT NULL,
-  `responsavel_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_Dependente_Cliente1_idx` (`responsavel_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Dependente_Cliente1`
-    FOREIGN KEY (`responsavel_id`)
-    REFERENCES `cliente` (`id`));
-
-
--- -----------------------------------------------------
--- Table `mensagem`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mensagem` (
-  `id` INT NOT NULL,
-  `data` DATETIME NULL DEFAULT NULL,
-  `conteudo` VARCHAR(45) NULL DEFAULT NULL,
-  `chat_id` INT NOT NULL,
-  `cliente_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_Mensagem_Chat1_idx` (`chat_id` ASC) VISIBLE,
-  INDEX `fk_Mensagem_Cliente1_idx` (`cliente_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Mensagem_Chat1`
-    FOREIGN KEY (`chat_id`)
-    REFERENCES `chat` (`id`),
-  CONSTRAINT `fk_Mensagem_Cliente1`
-    FOREIGN KEY (`cliente_id`)
-    REFERENCES `cliente` (`id`));
-
-
--- -----------------------------------------------------
--- Table `trajeto`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `trajeto` (
-  `id` INT NOT NULL,
-  `escola` VARCHAR(45) NULL DEFAULT NULL,
-  `tipo` VARCHAR(45) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`));
-
--- -----------------------------------------------------
--- Table `trajetodependente`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `trajetodependente` (
-  `trajeto_id` INT NOT NULL,
-  `dependente_id` INT NOT NULL,
-  `endereco_id` INT NOT NULL,
-  PRIMARY KEY (`trajeto_id`, `dependente_id`, `endereco_id`),
-  INDEX `fk_Trajeto_has_Dependente_Dependente1_idx` (`dependente_id` ASC) VISIBLE,
-  INDEX `fk_Trajeto_has_Dependente_Trajeto1_idx` (`trajeto_id` ASC) VISIBLE,
-  INDEX `fk_TrajetoDependente_Endereco1_idx` (`endereco_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Trajeto_has_Dependente_Dependente1`
-    FOREIGN KEY (`dependente_id`)
-    REFERENCES `dependente` (`id`),
-  CONSTRAINT `fk_Trajeto_has_Dependente_Trajeto1`
-    FOREIGN KEY (`trajeto_id`)
-    REFERENCES `trajeto` (`id`),
-  CONSTRAINT `fk_TrajetoDependente_Endereco1`
-    FOREIGN KEY (`endereco_id`)
-    REFERENCES `endereco` (`id`));
-
+	`id` INT NOT NULL,
+	`nome` VARCHAR(45) NULL,
+	`data_nascimento` DATE NULL,
+	`escola` VARCHAR(45) NULL,
+	`serie` VARCHAR(45) NULL,
+	`responsavel_id` INT NOT NULL,
+	PRIMARY KEY (`id`),
+	INDEX `fk_dependente_usuario_idx` (`responsavel_id` ASC) VISIBLE,
+	CONSTRAINT `fk_dependente_usuario`
+		FOREIGN KEY (`responsavel_id`)
+		REFERENCES `Usuario` (`id`)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION
+);
+    
 -- -----------------------------------------------------
 -- Table `transporte`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `transporte` (
-  `id` INT NOT NULL,
-  `placa` VARCHAR(45) NULL DEFAULT NULL,
-  `cnpj` CHAR(14) NULL DEFAULT NULL,
-  `cnh` CHAR(9) NULL DEFAULT NULL,
-  `crm` VARCHAR(15) NULL DEFAULT NULL,
-  `crmc` VARCHAR(20) NULL DEFAULT NULL,
-  `motorista_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_Transporte_Cliente_idx` (`motorista_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Transporte_Cliente`
-    FOREIGN KEY (`motorista_id`)
-    REFERENCES `cliente` (`id`));
-    
+	`id` INT NOT NULL,
+	`placa` VARCHAR(45) NULL DEFAULT NULL,
+	`cnpj` CHAR(14) NULL DEFAULT NULL,
+	`cnh` CHAR(9) NULL DEFAULT NULL,
+	`crm` VARCHAR(15) NULL DEFAULT NULL,
+	`crmc` VARCHAR(20) NULL DEFAULT NULL,
+	`motorista_id` INT NOT NULL,
+	PRIMARY KEY (`id`),
+	INDEX `fk_transporte_usuario_idx` (`motorista_id` ASC) VISIBLE,
+	CONSTRAINT `fk_transporte_usuario`
+		FOREIGN KEY (`motorista_id`)
+		REFERENCES `usuario` (`id`)
+);
+
+-- -----------------------------------------------------
+-- Table `endereco`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `endereco` (
+	`id` INT NOT NULL,
+	`latitude` VARCHAR(45) NULL DEFAULT NULL,
+	`longitude` VARCHAR(45) NULL DEFAULT NULL,
+	`cep` CHAR(8) NULL DEFAULT NULL,
+	`numero` DECIMAL(4,0) NULL DEFAULT NULL,
+	`complemento` VARCHAR(45) NULL DEFAULT NULL,
+	`usuario_id` INT NOT NULL,
+	PRIMARY KEY (`id`),
+	INDEX `fk_endereco_usuario_idx` (`usuario_id` ASC) VISIBLE,
+	CONSTRAINT `fk_endereco_usuario`
+		FOREIGN KEY (`usuario_id`)
+		REFERENCES `usuario` (`id`)
+);
+
 -- -----------------------------------------------------
 -- Table `escola`
 -- -----------------------------------------------------
@@ -323,3 +87,78 @@ CREATE TABLE IF NOT EXISTS `escola` (
   CONSTRAINT `fk_escola_endereco`
     FOREIGN KEY (`endereco_id`)
     REFERENCES `endereco` (`id`));
+
+-- -----------------------------------------------------
+-- Table `trajeto`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `trajeto` (
+	`id` INT NOT NULL,
+	`tipo` VARCHAR(45) NULL DEFAULT NULL,
+	`dia_semana` VARCHAR(45) NULL DEFAULT NULL,
+	`escola_id` VARCHAR(45) NULL DEFAULT NULL,
+	PRIMARY KEY (`id`),
+	INDEX `fk_trajeto_escola_idx` (`escola_id` ASC) VISIBLE,
+	CONSTRAINT `fk_trajeto_escola`
+		FOREIGN KEY (`escola_id`)
+		REFERENCES `escola` (`id`)
+);
+
+-- -----------------------------------------------------
+-- Table `rota`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `rota` (
+  `trajeto_id` INT NOT NULL,
+  `dependente_id` INT NOT NULL,
+  `endereco_id` INT NOT NULL,
+  PRIMARY KEY (`trajeto_id`, `dependente_id`, `endereco_id`),
+  INDEX `fk_rota_trajeto_idx` (`dependente_id` ASC) VISIBLE,
+  INDEX `fk_rota_dependente_idx` (`trajeto_id` ASC) VISIBLE,
+  INDEX `fk_rota_endereco_idx` (`endereco_id` ASC) VISIBLE,
+  CONSTRAINT `fk_rota_dependente`
+    FOREIGN KEY (`dependente_id`)
+    REFERENCES `dependente` (`id`),
+  CONSTRAINT `fk_rota_trajeto`
+    FOREIGN KEY (`trajeto_id`)
+    REFERENCES `trajeto` (`id`),
+  CONSTRAINT `fk_rota_endereco`
+    FOREIGN KEY (`endereco_id`)
+    REFERENCES `endereco` (`id`));
+
+    
+-- -----------------------------------------------------
+-- Table `chat`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `chat` (
+	`id` INT NOT NULL,
+	`responsavel_id` INT NOT NULL,
+	`motorista_id` INT NOT NULL,
+	PRIMARY KEY (`id`, `motorista_id`, `responsavel_id`),
+	INDEX `fk_chat_responsavel_idx` (`responsavel_id` ASC) VISIBLE,
+	INDEX `fk_chat_motorista_idx` (`motorista_id` ASC) VISIBLE,
+	CONSTRAINT `fk_chat_responsavel`
+		FOREIGN KEY (`responsavel_id`)
+		REFERENCES `usuario` (`id`),
+	CONSTRAINT `fk_chat_motorista`
+		FOREIGN KEY (`motorista_id`)
+		REFERENCES `usuario` (`id`)
+);
+
+-- -----------------------------------------------------
+-- Table `mensagem`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mensagem` (
+	`id` INT NOT NULL,
+	`data` DATETIME NULL,
+	`conteudo` VARCHAR(45) NULL,
+	`chat_id` INT NOT NULL,
+	`usuario_id` INT NOT NULL,
+	PRIMARY KEY (`id`),
+	INDEX `fk_mensagem_chat_idx` (`chat_id` ASC) VISIBLE,
+	INDEX `fk_mensagem_usuario_idc` (`usuario_id` ASC) VISIBLE,
+	CONSTRAINT `fk_mensagem_chat`
+		FOREIGN KEY (`chat_id`)
+		REFERENCES `chat` (`id`),
+	CONSTRAINT `fk_mensagem_usuario`
+		FOREIGN KEY (`usuario_id`)
+		REFERENCES `usuario` (`id`)
+);
